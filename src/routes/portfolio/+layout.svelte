018c045e84b5ac0828e2054abdafd838f3df1983
Vinicius-Brito-Costa <vinibiatriz@gmail.com>
2023-07-21 18:57:30 -0300

    Vercel config and analytics

--- src/routes/portfolio/+layout.svelte
+++ src/routes/portfolio/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
     import data from "$lib/portfolio/common.json"
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    inject({ mode: dev ? 'development' : 'production' });
 </script>
 <style>
     @font-face {
