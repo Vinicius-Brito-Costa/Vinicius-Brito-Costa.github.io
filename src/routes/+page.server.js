f87c326fb05e35f33e62a9344e0c1064f92ba93b
Vinicius <39202981+Vinicius-Brito-Costa@users.noreply.github.com>
2023-12-12 23:08:23 -0300

    Redirect

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,5 +3,5 @@ import { redirect } from '@sveltejs/kit';
 // If the user access the default path it will be redirected
 // to the portfolio route
 export function load() {
-  throw redirect(302, '/portfolio');
+  throw redirect(302, '/portfolio/about');
 }
