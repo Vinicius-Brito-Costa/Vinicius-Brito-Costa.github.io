86e32872cdf47ddcd6d243e33235dd37f7db97e5
Vinicius <39202981+Vinicius-Brito-Costa@users.noreply.github.com>
2024-07-04 00:54:25 -0300

    Changes
    + Profile Picture based on github
    ~ Description

--- src/lib/portfolio/components/content/about/About.svelte
+++ src/lib/portfolio/components/content/about/About.svelte
@@ -7,23 +7,47 @@
     article {
         align-self: right;
         width: 600px;
-        padding-left: 10px;
+        height: 600px;
         text-align: justify;
         align-items: center;
         font-family: var(--hack-font);
-        border-left: 3px solid var(--main-color);
         color: var(--secondary-color);
         display: flex;
+        flex-direction: column;
     }
 
     article p {
-        margin: 0;
+        margin-bottom: auto;
         padding: 0;
+        text-align: justify;
+        padding-left: 10px;
+        border-left: 3px solid var(--main-color);
     }
-
+    
+    .container {
+        width: 100%;
+        flex-grow: 1;
+        display: flex;
+    }
+    .container img {
+        margin: auto;
+        height: 100%;
+        object-fit: contain;
+        image-rendering: pixelated;
+        filter: grayscale(100%);
+        mix-blend-mode: multiply;
+    }
+    .tint{
+        border-radius: 100%;
+        overflow: hidden;
+        margin: 0 auto;
+        border: 3px solid var(--main-color);
+        background-color: var(--main-color);
+    }
     @media only screen and (max-width: 1180px) {
         article {
             width: calc(100% - 13px);
+            height: 450px;
             padding: 10px 10px;
         }
     }
@@ -33,7 +57,15 @@
     <meta name="description" content={data.meta.description} />
 </svelte:head>
 <article>
-    <p>
-        {data.about.description}
-    </p>
+    <div class="container">
+        <div class="tint">
+            <img src={data.about.profile_picture.img_url} alt={data.about.profile_picture.alt} />
+        </div>
+    </div>
+    
+    <div class="container">
+        <p>
+            {data.about.description}
+        </p>
+    </div>
 </article>
